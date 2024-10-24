105355a6e019f9a143b42e6639fc42770d87fd03
Brian Pluhar <pluharbrian@gmail.com>
2024-10-24 14:03:43 -0400

    Escaped char

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
    <>
-    <h1 className="text-2xl font-bold p-4">Access with GET Request to <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold text-red-600">/api/hello</code>, or click <Link className="text-blue-600" href="/api/hello">here</Link> if you're lazy.</h1>
+    <h1 className="text-2xl font-bold p-4">Access with GET Request to <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold text-red-600">/api/hello</code>, or click <Link className="text-blue-600" href="/api/hello">here</Link> if you&apos;re lazy.</h1>
    </>
   );
 }
