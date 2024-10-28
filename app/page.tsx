33fac4c5b8087bff2af5134ba8df0cb00c8963df
Brian Pluhar <pluharbrian@gmail.com>
2024-10-28 15:15:22 -0400

    Added exampe to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -6,10 +6,30 @@ export default function Home() {
       <h1 className="text-2xl font-bold p-4">
         Access with POST Request to{" "}
         <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold text-red-600">
-          /api/hello
-        </code>, or click{" "}
-        <Link className="text-blue-600" href="/api/hello">here</Link>{" "}
-        if you&apos;re lazy.
+          /api/avatar
+        </code>
+
+      <div className="mt-4 text-base">
+        <p className="mb-2 font-semibold">Example POST request:</p>
+          <pre className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded">
+            {`fetch('https://ez-avatar.vercel.app/api/avatar', {
+  method: 'POST',
+  body: FormData {
+    'bg-color': 'blue-500',    // Optional: Tailwind color
+    'fg-color': 'white',       // Optional: Text color
+    'img-size': '128',         // Required: 1-1000px
+    'font-size': 'lg',         // Optional: sm|md|lg|xl
+    'chars': 'BP',             // Required: Text to display
+    'rounded': 'true'          // Optional: true|false
+  }
+})`}
+          </pre>
+      </div>
+
+      <div className="mt-4 text-base">
+        <p className="mb-2 font-semibold">Returned Image:</p>
+        <Image src="/response.webp" alt="bp" width={64} height={64} />
+      </div>
       </h1>
     </>
   );
